2284b09a382d3807bee5d28d9b22050aa864a551
AnatoliyKurilo <anatolijkurilo40@gmail.com>
2024-04-12 15:55:38 +0600

    Добавьте файлы проекта.

---
+++ PR_TEST_12_04_24/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_TEST_12_04_24
+{
+    public class Program
+    {
+        public static string Ur(double a, double b, double c) {
+            
+            double x1, x2;
+
+            var discriminant = Math.Pow(b, 2) - 4 * a * c;
+            if (discriminant < 0)
+            {
+                return("Квадратное уравнение не имеет корней");
+
+            }
+            else
+            {
+                if (discriminant == 0) 
+                {
+                    x2 = x1 = (-b) / (2 * a);
+                }
+                else 
+                {
+                    x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                }
+                
+                return($"x1 = {x1}; x2 = {x2}");
+            }
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Решение квадратного уравнения");
+            //ввод данных
+            Console.Write("a = ");
+            var a = double.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            var b = double.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            var c = double.Parse(Console.ReadLine());
+
+            Ur(a,b,c);
+            
+
+            Console.WriteLine("Для выхода нажмите любую клавишу...");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PR_TEST_12_04_24;
+using System.IO;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double a=1;
+            double b=1;
+            double c=1;
+            string Expected = "Квадратное уравнение не имеет корней";
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                PR_TEST_12_04_24.Program.Ur(a, b, c);
+
+                var result = Program.Ur(a, b, c); //sw.ToString().Trim();
+
+                Assert.AreEqual(Expected, result);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double a = 1;
+            double b = 2;
+            double c = 0;
+            string Expected = "x1 = 0; x2 = -2";
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                PR_TEST_12_04_24.Program.Ur(a, b, c);
+
+                //var result = sw.ToString().Trim();
+                var result = Program.Ur(a, b, c);
+                Assert.AreEqual(Expected, result);
+            }
+        }
+
+        [TestMethod]
+
+        public void TestMethod3()
+        {
+            double a = 1;
+            double b = 2;
+            double c = 1;
+            string Expected = "x1 = -1; x2 = -1";
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                PR_TEST_12_04_24.Program.Ur(a, b, c);
+
+                //var result = sw.ToString().Trim();
+                var result = Program.Ur(a, b, c);
+                Assert.AreEqual(Expected, result);
+            }
+        }
+
+    }
+}
